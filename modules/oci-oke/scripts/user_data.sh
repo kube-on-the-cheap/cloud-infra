2442938c3664809c3b1eaec5e69558fbc00ef063
Marco Bulgarini <marco.bulgarini@gmail.com>
2025-12-27 10:05:05 +0100

    feat: set cap on journald
    Signed-off-by: Marco Bulgarini <marco.bulgarini@gmail.com>

--- modules/oci-oke/scripts/user_data.sh
+++ modules/oci-oke/scripts/user_data.sh
@@ -16,8 +16,16 @@ echo '{
 export OCI_CLI_AUTH=instance_principal
 oci compute instance update --instance-id "$(oci-instanceid)" --agent-config file://./agent_config.json --force
 
-# Enable persistent journal logging
-mkdir -p /var/log/journal
+# Enable persistent journal logging with size limits
+mkdir -p {/var/log/journal,/etc/systemd/journald.conf.d}
+cat > /etc/systemd/journald.conf.d/10-disk-usage.conf <<EOF
+[Journal]
+SystemMaxUse=5G
+SystemKeepFree=10G
+MaxRetentionSec=7day
+MaxFileSec=1day
+EOF
+systemctl restart systemd-journald
 
 # Expand the disk
 growpart /dev/oracleoci/oraclevda 3
